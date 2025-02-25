85a07fd58935e63155fced372523d79ebc5461df
joel bispo <joelbispo04@hotmail.com>
2025-02-25 15:20:03 -0300

    organização do login

--- auto_moto/src/Pages/Login.tsx
+++ auto_moto/src/Pages/Login.tsx
@@ -1,45 +1,47 @@
-import { Box, Button, Card, CardContent, Container, Grid2, TextField, Typography } from "@mui/material"
-import AppBarComponent from "../components/AppBarComponent"
+import { AppBar, Box, Button, Card, CardContent, Grid2, TextField, Toolbar, Typography } from "@mui/material"
+import SportsMotorsportsIcon from '@mui/icons-material/SportsMotorsports';
 import ButtonMenu from "../components/ButtonMenu";
 
 
 const Login = () => {
 
 
-    return(
+
+    return (
         <>
-        <AppBarComponent/>
-        <Container>
+            <AppBar position="fixed"
+                sx={{
+                    color: "#1a1a1a",
+                    backgroundColor: 'white',
+                    boxShadow: 'none',
+                    border: '1px solid'
+                }}>
+                <Toolbar>
+                    <SportsMotorsportsIcon sx={{ padding: 1, color: '#3b06b6' }} fontSize="large" />
+                    <Typography variant="h6" fontWeight={800} flexGrow={1}>
+                        AutoMotoShop
+                    </Typography>
+                    <Button variant="contained"  sx={{ backgroundColor: '#3b06b6', color: "fff" }}>Cadastrar</Button>
+                </Toolbar>
+            </AppBar>
             <Grid2 size={{ xs: 12, md: 12, lg: 12 }}>
+                <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
                 <ButtonMenu/>
-                <Card sx={{ mt: 2, border: '1px solid', display: 'flex'}}>
-                    <CardContent sx={{ width: '50%'}}>
-                        <Box sx={{ display: 'flex', flexDirection: 'column'}}>
-                        <Typography variant='h4'>Cadastrar-se</Typography>
-                        <Typography variant="h6">
-                            Digite seu nome, email e senha
-                        </Typography>
-                        <TextField label='nome' >Nome</TextField>
-                        <TextField label='email'>Email</TextField>
-                        <TextField label='senha'>Senha</TextField>
-                        <Button variant="contained">Cadastrar</Button>
-                        </Box>
-                    </CardContent>
-                    <CardContent sx={{ width: '50%'}}>
-                        <Box sx={{ display: 'flex', flexDirection: 'column'}}>
-                        <Typography variant='h4'>Entrar</Typography>
-                        <Typography variant="h6">
-                            Digite seu nome, email e senha
-                        </Typography>
-                        <TextField label='nome' >Nome</TextField>
-                        <TextField label='email'>Email</TextField>
-                        <TextField label='senha'>Senha</TextField>
-                        <Button variant="contained">Entrar</Button>
-                        </Box>
-                    </CardContent>
-                </Card>
+                
+                    <Card sx={{ mt: 2, border: '1px solid', maxWidth: 400 }}>
+                        <CardContent>
+                            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', padding: 5 }}>
+                                <Typography variant="h5">
+                                    Digite seu email e senha
+                                </Typography>
+                                <TextField label='email' sx={{ mt: 2 }}>Email</TextField>
+                                <TextField label='senha' sx={{ mt: 2 }}>Senha</TextField>
+                                <Button variant="contained" sx={{ backgroundColor: '#3b06b6', mt: 2 }}>Entrar</Button>
+                            </Box>
+                        </CardContent>
+                    </Card>
+                </Box>
             </Grid2>
-        </Container>
         </>
     )
 }
