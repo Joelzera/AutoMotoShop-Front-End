3921115d18499bf6ab6e5c9cff1f6174f8477211
joel bispo <joelbispo04@hotmail.com>
2025-03-07 11:04:30 -0300

    alterações

--- auto_moto/src/components/AppBarComponent.tsx
+++ auto_moto/src/components/AppBarComponent.tsx
@@ -1,4 +1,4 @@
-import { AppBar,  Button,  Toolbar, Typography } from "@mui/material";
+import { AppBar,  Box,  Button,  Toolbar, Typography } from "@mui/material";
 import SportsMotorsportsIcon from '@mui/icons-material/SportsMotorsports';
 
 import { useNavigate } from "react-router-dom";
@@ -20,9 +20,13 @@ const AppBarComponent = () => {
             }}>
             <Toolbar>
                 <SportsMotorsportsIcon sx={{ padding: 1, color: '#3b06b6' }} fontSize="large" />
-                <Typography variant="h6" fontWeight={800} flexGrow={1}>
+                <Typography variant="h6" fontWeight={800} flexGrow={0.5}>
                     AutoMotoShop
                 </Typography>
+                <Box flexGrow={0.6}>
+                <Button variant="text" size="small" color="inherit" sx={{ padding: 1}} onClick={() => navigate('/comprar')}>Comprar</Button>
+                <Button variant="text" size="small" color="inherit" sx={{ padding: 1}} onClick={() => navigate('/vender')}>Vender</Button>
+                </Box>
                 <Button variant="contained" size="small" sx={{ backgroundColor: '#3b06b6', color: "fff", fontFamily: 'cursive' }}  onClick={() => navigate('/login')}>Entrar</Button>
             </Toolbar>
         </AppBar>
