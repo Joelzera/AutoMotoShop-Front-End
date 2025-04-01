75f963614d091767dcf9e09274cf5c100a93903a
Joel Bispo <joelbispo04@hotmail.com>
2025-04-01 14:51:52 -0300

    começando a parte de upload de arquivos

--- auto_moto/src/Pages/Sell.tsx
+++ auto_moto/src/Pages/Sell.tsx
@@ -1,10 +1,11 @@
-import { AppBar, Box, Button, Card, CardContent, Container, FormControl,  Grid2, MenuItem, Select, TextField, Toolbar, Typography } from "@mui/material"
+import { AppBar, Box, Button, Card, CardContent, CardMedia, Container, FormControl, Grid2, MenuItem, Select, styled, TextField, Toolbar, Typography } from "@mui/material"
 import SportsMotorsportsIcon from '@mui/icons-material/SportsMotorsports';
 import { useNavigate } from "react-router-dom";
-import teste from '../../img/TesteCapa.jpg'
 import { useState } from "react";
 import axios from "axios";
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import AddBoxIcon from '@mui/icons-material/AddBox';
+import teste from '../../img/TesteCapa.jpg'
 
 const Sell = () => {
 
@@ -56,6 +57,19 @@ const Sell = () => {
         { id: 5, tipo: 'suv' },
     ]
 
+    const VisuallyHiddenInput = styled('input')({
+        clip: 'rect(0 0 0 0)',
+        clipPath: 'inset(50%)',
+        height: 1,
+        overflow: 'hidden',
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        whiteSpace: 'nowrap',
+        width: 1,
+    });
+
+
     return (
         <Grid2 sx={{ backgroundColor: '#ECEDF2', height: '100%', mt: -2 }}>
             <AppBar position="fixed"
@@ -70,27 +84,41 @@ const Sell = () => {
                         AutoMotoShop
                     </Typography>
                     <Button variant="contained"
-                            onClick={() => navigate('/')}
-                            startIcon={<ArrowBackIcon/>} 
-                            sx={{ backgroundColor: '#3b06b6', color: "fff" }}>Menu</Button>
-                    <Button variant="contained" 
-                            onClick={() => navigate('/login')} 
-                            sx={{ backgroundColor: '#3b06b6', color: "fff", marginLeft: 1 }}>Entrar</Button>
+                        onClick={() => navigate('/')}
+                        startIcon={<ArrowBackIcon />}
+                        sx={{ backgroundColor: '#3b06b6', color: "fff" }}>Menu</Button>
+                    <Button variant="contained"
+                        onClick={() => navigate('/login')}
+                        sx={{ backgroundColor: '#3b06b6', color: "fff", marginLeft: 1 }}>Entrar</Button>
                 </Toolbar>
             </AppBar>
             <Container>
                 <Grid2 container justifyContent="center">
-                    <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: '100%',  mt: 12 }}>
+                    <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: '100%', mt: 12 }}>
                         <Card sx={{ width: '80%' }}>
-                            <img src={teste} />
+                            <CardMedia image={teste} sx={{ height: 500 }}/>
+                            <Button
+                                component="label"
+                                role={undefined}
+                                variant="outlined"
+                                tabIndex={-1}
+                                startIcon={<AddBoxIcon />}
+                            >
+                                Adicionar Fotos
+                                <VisuallyHiddenInput
+                                    type="file"
+                                    onChange={(event) => console.log(event.target.files)}
+                                    multiple
+                                />
+                            </Button>
                             <CardContent>
                                 <form onSubmit={handleChange} style={{ display: 'flex', flexDirection: 'column' }}>
                                     <Grid2 container spacing={2}>
-                                        <Grid2  size={{xs:12, sm:6}} >
+                                        <Grid2 size={{ xs: 12, sm: 6 }} >
                                             <Typography variant="body1">Nome</Typography>
                                             <TextField fullWidth value={car.nome} onChange={(e) => setCar({ ...car, nome: e.target.value })} />
                                         </Grid2>
-                                        <Grid2  size={{xs:12, sm:6}}>
+                                        <Grid2 size={{ xs: 12, sm: 6 }}>
                                             <Typography variant="body1">Marca</Typography>
                                             <FormControl fullWidth>
                                                 <Select value={car.marca} onChange={(e) => setCar({ ...car, marca: e.target.value })}>
@@ -102,19 +130,19 @@ const Sell = () => {
                                                 </Select>
                                             </FormControl>
                                         </Grid2>
-                                        <Grid2 size={{xs:12, sm:6}}>
+                                        <Grid2 size={{ xs: 12, sm: 6 }}>
                                             <Typography variant="body1">Ano</Typography>
                                             <TextField fullWidth value={car.ano} onChange={(e) => setCar({ ...car, ano: e.target.value })} />
                                         </Grid2>
-                                        <Grid2 size={{xs:12, sm:6}}>
+                                        <Grid2 size={{ xs: 12, sm: 6 }}>
                                             <Typography variant="body1">Motor</Typography>
                                             <TextField fullWidth value={car.motor} onChange={(e) => setCar({ ...car, motor: e.target.value })} />
                                         </Grid2>
-                                        <Grid2 size={{xs:12, sm:6}}>
+                                        <Grid2 size={{ xs: 12, sm: 6 }}>
                                             <Typography variant="body1">Modelo</Typography>
                                             <TextField fullWidth value={car.modelo} onChange={(e) => setCar({ ...car, modelo: e.target.value })} />
                                         </Grid2>
-                                        <Grid2 size={{xs:12, sm:6}}>
+                                        <Grid2 size={{ xs: 12, sm: 6 }}>
                                             <Typography variant="body1">Tipo</Typography>
                                             <FormControl fullWidth>
                                                 <Select value={car.tipo} onChange={(e) => setCar({ ...car, tipo: e.target.value })}>
@@ -126,23 +154,23 @@ const Sell = () => {
                                                 </Select>
                                             </FormControl>
                                         </Grid2>
-                                        <Grid2 size={{xs:12, sm:6}}>
+                                        <Grid2 size={{ xs: 12, sm: 6 }}>
                                             <Typography variant="body1">Cor</Typography>
                                             <TextField fullWidth value={car.cor} onChange={(e) => setCar({ ...car, cor: e.target.value })} />
                                         </Grid2>
-                                        <Grid2 size={{xs:12, sm:6}}>
+                                        <Grid2 size={{ xs: 12, sm: 6 }}>
                                             <Typography variant="body1">Câmbio</Typography>
                                             <TextField fullWidth value={car.cambio} onChange={(e) => setCar({ ...car, cambio: e.target.value })} />
                                         </Grid2>
-                                        <Grid2 size={{xs:12, sm:6}}>
+                                        <Grid2 size={{ xs: 12, sm: 6 }}>
                                             <Typography variant="body1">Combustível</Typography>
                                             <TextField fullWidth value={car.combustivel} onChange={(e) => setCar({ ...car, combustivel: e.target.value })} />
                                         </Grid2>
-                                        <Grid2 size={{xs:12, sm:6}}>
+                                        <Grid2 size={{ xs: 12, sm: 6 }}>
                                             <Typography variant="body1">Km</Typography>
                                             <TextField fullWidth value={car.quilometragem} onChange={(e) => setCar({ ...car, quilometragem: e.target.value })} />
                                         </Grid2>
-                                        <Grid2 size={{xs:12, sm:6}}>
+                                        <Grid2 size={{ xs: 12, sm: 6 }}>
                                             <Typography variant="body1">Preço</Typography>
                                             <TextField fullWidth value={car.preço} onChange={(e) => setCar({ ...car, preço: e.target.value })} />
                                         </Grid2>
